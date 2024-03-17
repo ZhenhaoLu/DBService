6e320d62ce49a0db338d87bea83b770c44c37ed5
Zhenhao Lu <zhenhao@usc.edu>
2024-03-16 21:31:49 -0700

    Update

--- src/main/java/com/example/dbservice/controller/HibernateController.java
+++ src/main/java/com/example/dbservice/controller/HibernateController.java
@@ -6,10 +6,9 @@ import com.example.dbservice.service.impl.HibernateService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-@Controller
+@RestController
 @RequestMapping("/hibernate")
 public class HibernateController {
 
